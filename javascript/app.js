58f2e391e09648d6ead73e91070c7885e574ed5b
Alan Leverenz <awleverenz@aol.com>
2020-08-07 18:19:09 -0400

    installed Browserify to load require() files into the browser

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,8 @@
-const googleMapsApikey = 'AIzaSyAAXRzfOEywj2IQRnUNL42XHdT43bu0VUg';
+// const googleMapsApikey = 'AIzaSyAAXRzfOEywj2IQRnUNL42XHdT43bu0VUg';
 
 // import env file
-// require('dotenv').config();
-// const googleMapsApikey = process.env.googleMapsApikey;
+require('dotenv').config();
+let googleMapsApikey = process.env.googleMapsApikey;
 
 // AJAX SETUP
 $.ajaxSetup({
