e0fea2491fb6d51b51ff77fb297451d806c50e35
Alan Leverenz <awleverenz@aol.com>
2020-08-13 18:02:07 -0400

    updated news.js

--- javascript/news-bundle.js
+++ javascript/news-bundle.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 // require api key data
 const chain = require('./ourApiKeys');
 
@@ -55,3 +56,15 @@ $(document).ready(function () {
     }); // end then
   }); // end onclick event
 }); // end document.ready
+
+},{"./ourApiKeys":2}],2:[function(require,module,exports){
+const keys = {
+  googleMapsApikey: 'AIzaSyAAXRzfOEywj2IQRnUNL42XHdT43bu0VUg',
+  nytApiKey: 'GAN5Vuqp6dyl6vNHxlmwbLizhaZMVVf6',
+  fixerApiKey: '231d60d3c3c46df524fec57f238b3a02',
+  firebaseApiKey: 'AIzaSyD5TgHMFez2lODS4UgYrIobJSWGPtf0bI8',
+};
+
+exports.keys = keys;
+
+},{}]},{},[1]);
--- javascript/ourApiKeys.js
+++ javascript/ourApiKeys.js
@@ -1,8 +1,8 @@
 const keys = {
   googleMapsApikey: 'AIzaSyAAXRzfOEywj2IQRnUNL42XHdT43bu0VUg',
-  nytApiKey = 'GAN5Vuqp6dyl6vNHxlmwbLizhaZMVVf6',
-  fixerApiKey = '231d60d3c3c46df524fec57f238b3a02',
-  firebaseApiKey: 'AIzaSyD5TgHMFez2lODS4UgYrIobJSWGPtf0bI8'
+  nytApiKey: 'GAN5Vuqp6dyl6vNHxlmwbLizhaZMVVf6',
+  fixerApiKey: '231d60d3c3c46df524fec57f238b3a02',
+  firebaseApiKey: 'AIzaSyD5TgHMFez2lODS4UgYrIobJSWGPtf0bI8',
 };
 
 exports.keys = keys;
